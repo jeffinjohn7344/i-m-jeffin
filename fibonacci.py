fedb10ec32b8702da81f7f0a22d0c164463b9569
Ashikpaul07 <ashikpaul@karunya.edu.in>
2025-02-25 20:41:48 +0530

    adding

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,15 @@
+#assigning variable
 n = 10
 num1 = 0
 num2 = 1
 next_number = num2  
 count = 1
-
+#loopin using while loop
 while count <= n:
     print(next_number, end=" ")
+#logic
     count += 1
     num1, num2 = num2, next_number
     next_number = num1 + num2
+#printing result
 print()
